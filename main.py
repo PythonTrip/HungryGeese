e7542b25bc3c8dd3d7f6ff0730c172142280e045
PyTrip <thealexframe@gmail.com>
2021-07-13 19:50:34 +0300

    Add elite selected and calc reward

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from functools import lru_cache
 from typing import Tuple
 from time import time
 from abc import abstractmethod, ABC
-from random import choice
+from random import choice, random
 import torch
 import numpy as np
 
@@ -20,6 +20,48 @@ classifier = torch.nn.Sequential(
     torch.nn.ReLU(),
     torch.nn.Linear(32, n_actions)
 )
+optim = torch.optim.Adam(classifier.parameters(), lr=1e-3)
+loss = torch.nn.MSELoss()
+
+
+def calc_td_loss(states, actions, rewards, next_states, is_done, gamma=0.99, check_shapes=False):
+    states = torch.tensor(states, dtype=torch.float32)  # shape: [batch_size, state_size]
+    actions = torch.tensor(actions, dtype=torch.long)  # shape: [batch_size]
+    rewards = torch.tensor(rewards, dtype=torch.float32)  # shape: [batch_size]
+    next_states = torch.tensor(next_states, dtype=torch.float32)  # shape: [batch_size, state_size]
+
+
+def select_elites(states_batch, actions_batch, rewards_batch):
+    max_rew = np.argmax(rewards_batch, 1)
+
+    elite_state = []
+    elite_actions = []
+    for i, mr in enumerate(max_rew):
+        elite_state.append(states_batch[i][mr])
+        elite_actions.append(actions_batch[i][mr])
+    return elite_state, elite_actions
+
+
+def calc_reward(session, count_geese):
+    rewards = [0] * count_geese
+    len_geese = [1] * count_geese
+
+    for k in range(count_geese):
+        for j in range(len(session)):
+            current_length = len(session[j][0].observation.geese[k])
+
+            if current_length == 0:
+                rewards[k] -= 100
+
+            rewards[k] -= 10
+            if current_length > len_geese[k]:
+                rewards[k] += 100
+                len_geese[k] = current_length
+
+            elif current_length < len_geese[k]:
+                rewards[k] -= 50
+                len_geese[k] = current_length
+    return rewards
 
 
 def base_point(rc):
@@ -90,6 +132,10 @@ class AAgent(ABC):
         self.all_geese = []
         self.food = []
 
+        self.states_batch = []
+        self.actions_batch = []
+        self.epsilon = 0.1
+
     def get_base(self, obs_dict, config_dict):
         self.observation = Observation(obs_dict)
         self.configuration = Configuration(config_dict)
@@ -111,6 +157,9 @@ class BaseAgent(AAgent):
 
         act = choice(list(self.actions.possible_actions(last_action=self.last_act)))
         self.last_act = act
+
+        self.states_batch.append(np.array([self.player_head, *self.food]) / 77)
+        self.actions_batch.append(act)
         return act
 
 
@@ -118,71 +167,50 @@ class MLPAgent(AAgent):
     def agent(self, obs_dict, config_dict):
         self.get_base(obs_dict, config_dict)
 
-        # player_row, player_column = row_col(self.player_head, self.configuration.columns)
-
-        # food_row, food_column = row_col(self.food, self.configuration.columns)
+        state = np.array([self.player_head, *self.food]) / 77
+        self.states_batch.append(state)
 
         act = choice(list(self.actions.actions_by_board(self.all_geese, self.player_head, self.last_act)))
         if len(act) == 0:
-            return choice(self.actions.actions)
+            act = choice(self.actions.actions)
+            self.actions_batch.append(act)
+            return act
 
         q_value = classifier(torch.tensor([self.player_head, *self.food]) / 77).detach().numpy().tolist()
 
         for q in sorted(q_value, reverse=True):
             if self.actions.actions[q_value.index(q)] in act:
-                self.last_act = act
+                self.last_act = act if random() > self.epsilon else choice(self.actions.actions)
                 return self.actions.actions[q_value.index(q)]
 
         raise Exception("None act")
 
 
-def generate_session(env, agents):
-    states, actions = [], []
-    total_reward = 0
-
+def generate_session(env, agents: AAgent):
     env.reset()
-    state = env.run(agents)
+    state = env.run([x.agent for x in agents])
 
-    print(calc_reward(state, len(agents)))
+    states = [x.states_batch for x in agents]
+    actions = [x.actions_batch for x in agents]
 
-
-def calc_reward(session, count_geese):
-    rewards = [0] * count_geese
-    len_geese = [1] * count_geese
-
-    for k in range(count_geese):
-        for j in range(len(session)):
-            current_length = len(session[j][0].observation.geese[k])
-
-            if current_length == 0:
-                rewards[k] -= 100
-
-            rewards[k] -= 10
-            if current_length > len_geese[k]:
-                rewards[k] += 100
-                len_geese[k] = current_length
-
-            elif current_length < len_geese[k]:
-                rewards[k] -= 50
-                len_geese[k] = current_length
-    print(rewards)
+    return states, actions, calc_reward(state, len(agents))
 
 
 base_agent = BaseAgent()
-# generate_session(env, [base_agent.agent, MLPAgent().agent, MLPAgent().agent])
-n_sessions = 1
-percentile = 50
+
+n_sessions = 2
+
 log = []
 
-for i in range(1):
-    sessions = [generate_session(env, [base_agent.agent, MLPAgent().agent, MLPAgent().agent]) for _ in
+for i in range(2):
+    sessions = [generate_session(env, [base_agent, MLPAgent(), MLPAgent()]) for _ in
                 range(n_sessions)]
 
-    # states_batch, actions_batch, rewards_batch = map(np.array, zip(*sessions))
+    states_batch, actions_batch, rewards_batch = map(np.array, zip(*sessions))
 
-    # elite_states, elite_actions = select_elites(states_batch, actions_batch, rewards_batch, percentile)
+    elite_states, elite_actions = select_elites(states_batch, actions_batch, rewards_batch)
 
-    # classifier.fit(elite_states, elite_actions)
+    optim.zero_grad()
 
     # show_progress(rewards_batch, log, percentile, reward_range=[0, np.max(rewards_batch)])
 
